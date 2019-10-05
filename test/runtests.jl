0ba36b0a29ea6c3d95b33a5507bdfb7ce425c4ad
Amga Baatarjav <amgalan_amg@yahoo.com>
2019-10-05 20:31:46 +0100

    funAdd is a linear function demonstration

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using MyExample
 using Test
 
+
 @testset "MyExample.jl" begin
     # Write your own tests here.
+    #2x + 3y
+    @test funAdd(2, 1) ==  7
+    @test funAdd(2, 3) ==  13
+    funAdd(2,1)
 end
